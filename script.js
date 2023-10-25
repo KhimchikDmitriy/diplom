35e113a3bdac6325d62dd561a8b8cc82a7c83bb5
Химчик Дмитрий <dimahima14@gmail.com>
2023-10-26 00:21:33 +0300

    meloch

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
-let colors = ["url(./img/1.jpg)", "url(./img/2.png)", "url(./img/3.png)"];
-let c = 0;
+// переменные
+let b = 0;
 
+// функция переключения фона хдэра
 function changeColor() {
+  let back = ["url(./img/1.jpg)", "url(./img/2.png)", "url(./img/3.png)"];
   let div = document.querySelector(".head");
-  div.style.background = colors[c];
-  c++;
-  if (c >= colors.length) {
-    c = 0;
+  div.style.background = back[b];
+  b++;
+  if (b > back.length) {
+    b = 0;
   }
 }
 setInterval(changeColor, 5000);
 
+// вставка кода для бокового меню
 fetch("./dop/canvas.html")
   .then((resp) => resp.text())
   .then((code) => {
     document.querySelector(".canvas").innerHTML = code;
   });
 
+// вставка кода для карточек примеров на заглавной странице
 fetch("./dop/examples.html")
   .then((resp) => resp.text())
   .then((code) => {
     document.querySelector(".examples").innerHTML = code;
   });
 
+// вставка кода для обратной связи
 fetch("./dop/feedback.html")
   .then((resp) => resp.text())
   .then((code) => {
